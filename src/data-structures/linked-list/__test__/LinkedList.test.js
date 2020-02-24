3779830045e2c36bf0bee753f59473547ce6e772
Huseyn <huseyn.mikayil95@gmail.com>
2020-02-24 18:53:18 +0400

    Add method was created

--- src/data-structures/linked-list/__test__/LinkedList.test.js
+++ src/data-structures/linked-list/__test__/LinkedList.test.js
@@ -8,9 +8,22 @@ describe('LinkedList', () => {
     expect(linkedList).not.toBeNull();
   });
 
+  it('should add node to linked list', () => {
+    const linkedList = new LinkedList();
+    expect(linkedList.Count).toBe(0);
+    linkedList.Add("Huseyn");
+    expect(linkedList.Count).toBe(1);
+  });
+
   it('should return index of node in linked list', () => {
+    const linkedList = new LinkedList();
+    linkedList.Add("Huseyn");
+    expect(linkedList.IndexOf("Huseyn")).toBe(0);
+  });
+
+  it('should return -1 of node in linked list', () => {
     const linkedList = new LinkedList();
     expect(linkedList.IndexOf("test")).toBe(-1);
   });
-  
+
 });

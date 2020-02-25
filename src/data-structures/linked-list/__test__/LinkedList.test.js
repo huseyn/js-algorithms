59e1b49c8fba8b5540f65f55c8a43865c07df169
Huseyn <huseyn.mikayil95@gmail.com>
2020-02-25 15:12:47 +0400

    remove method tests was added

--- src/data-structures/linked-list/__test__/LinkedList.test.js
+++ src/data-structures/linked-list/__test__/LinkedList.test.js
@@ -3,37 +3,64 @@ const LinkedList = require('../LinkedList');
 
 describe('LinkedList', () => {
 
-    it('should create empty linked list', () => {
-        const linkedList = new LinkedList();
-        expect(linkedList).not.toBeNull();
-    });
+  it('should create empty linked list', () => {
+    const linkedList = new LinkedList();
+    expect(linkedList).not.toBeNull();
+  });
 
-    it('should add node to linked list', () => {
-        const linkedList = new LinkedList();
-        expect(linkedList.Count).toBe(0);
-        linkedList.Add(1);
-        expect(linkedList.Count).toBe(1);
-        expect(linkedList.head).not.toBeNull();
-        expect(linkedList.tail).not.toBeNull();
-        linkedList.Add(2);
-        expect(linkedList.Count).toBe(2);
-        expect(linkedList.head.Element).toBe(1);
-        expect(linkedList.tail.Element).toBe(2);
-        linkedList.Add(3);
-        expect(linkedList.Count).toBe(3);
-        expect(linkedList.head.Element).toBe(1);
-        expect(linkedList.tail.Element).toBe(3);
-    });
+  it('should add node to linked list', () => {
+    const linkedList = new LinkedList();
+    expect(linkedList.Count).toBe(0);
+    linkedList.Add(1);
+    expect(linkedList.Count).toBe(1);
+    expect(linkedList.head).not.toBeNull();
+    expect(linkedList.tail).not.toBeNull();
+    linkedList.Add(2);
+    expect(linkedList.Count).toBe(2);
+    expect(linkedList.head.Element).toBe(1);
+    expect(linkedList.tail.Element).toBe(2);
+    linkedList.Add(3);
+    expect(linkedList.Count).toBe(3);
+    expect(linkedList.head.Element).toBe(1);
+    expect(linkedList.tail.Element).toBe(3);
+  });
 
-    it('should return index of node in linked list', () => {
-        const linkedList = new LinkedList();
-        linkedList.Add("Huseyn");
-        expect(linkedList.IndexOf("Huseyn")).toBe(0);
-    });
+  it('should return index of node in linked list', () => {
+    const linkedList = new LinkedList();
+    expect(linkedList.IndexOf(5)).toBe(-1);
+    linkedList.Add(1);
+    expect(linkedList.IndexOf(1)).toBe(0);
+  });
 
-    it('should return -1 of node in linked list', () => {
-        const linkedList = new LinkedList();
-        expect(linkedList.IndexOf("test")).toBe(-1);
-    });
+  it('should return -1 in linked list', () => {
+    const linkedList = new LinkedList();
+    expect(linkedList.Remove(1)).toBe(-1);
+  });
 
+  it('should return 0 in linked list and head&tail should be null', () => {
+    const linkedList = new LinkedList();
+    linkedList.Add(1);
+    expect(linkedList.Remove(1)).toBe(0);
+    expect(linkedList.head).toBeNull();
+    expect(linkedList.tail).toBeNull();
+  });
+
+  it('should return 0 in linked list and head&tail should be changed', ()=>{
+    const linkedList = new LinkedList();
+    linkedList.Add(1);
+    linkedList.Add(2);
+    expect(linkedList.Remove(1)).toBe(0);
+    expect(linkedList.head.Element).toBe(2);
+    expect(linkedList.tail.Element).toBe(2);
+  });
+
+  it('should return 1 in linked list and tail should not be changed', () => {
+    const linkedList = new LinkedList();
+    linkedList.Add(1);
+    linkedList.Add(2);
+    expect(linkedList.Remove(2)).toBe(1); 
+    expect(linkedList.head.Element).toBe(1);
+    expect(linkedList.head.Element).toBe(1);
+    expect(linkedList.tail.Element).toBe(1);
+  });
 });

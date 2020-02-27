39c12c7938a88ae4b937d29574b4c3e0e9d7908c
Huseyn <huseyn.mikayil95@gmail.com>
2020-02-27 17:41:33 +0400

    stack was implemented

--- src/data-structures/stack/Stack.js
+++ src/data-structures/stack/Stack.js
@@ -1,30 +1,51 @@
 class Stack {
 
-    constructor(){
+    constructor() {
         this.container = [];
+        this.count = 0;
     }
 
-    push() {
-
+    push(item) {
+        this.container[this.count] = item;
+        this.count++;
     }
 
     pop() {
-
+        this.count--;
+        return this.container.pop();
     }
 
     peek() {
-
+        return this.container[this.count - 1];
     }
 
     clear() {
-
+        this.count = 0;
+        this.container = [];
     }
 
     contains(item) {
-
+        let index = 0;
+        while (index < this.count) {
+            if (this.container[index] === item) {
+                return true;
+            }
+            index++;
+        }
+        return false;
     }
 
     toArray() {
-
+        return this.container;
     }
 }
+
+const stack=new Stack();
+stack.push(1);
+stack.push(2);
+stack.push(3);
+const a = stack.peek();
+const b = stack.pop();
+const c = stack.contains(2);
+const d = stack.toArray();
+stack.clear();

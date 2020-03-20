463a5e7e8832ec9a2713d4e1255aad64aa7cdc12
Huseyn Mikayil <huseyn-mikayil@mail.ru>
2020-03-20 20:26:53 +0400

    some unnecessary pieces of codes were removed in all functions

--- src/data-structures/hashtable/Hashtable.js
+++ src/data-structures/hashtable/Hashtable.js
@@ -25,93 +25,50 @@ class Hashtable {
     put(element) {
         const hashCode = this.hashCode(element);
         const node = new Node(element);
-        if (hashCode > this.length) {
-            const index = hashCode % this.length;
-            if (this.array[index]) {
-                node.NextNode = this.array[index];
-                this.array[index] = node;
-                this.count++;
-            } else {
-                this.array[index] = element;
-                this.count++;
-            }
+        const index = hashCode % this.length;
+        if (this.array[index]) {
+            node.NextNode = this.array[index];
+            this.array[index] = node;
+            this.count++;
         } else {
-            if (this.array[hashCode]) {
-                node.NextNode = this.array[hashCode];
-                this.array[hashCode] = node;
-                this.count++;
-            } else {
-                this.array[hashCode] = element;
-                this.count++;
-            }
+            this.array[index] = element;
+            this.count++;
         }
     }
 
     remove(element) {
         const hashCode = this.hashCode(element);
-        if (hashCode > this.length) {
-            const index = hashCode % this.length;
-            if (this.array[index]) {
-                if (this.array[index].Element === element) {
-                    this.array[index] = undefined;
-                    this.count--;
-                } else {
-                    while (this.array[index].NextNode) {
-                        if (this.array[index].NextNode === element) {
-                            this.array[index].NextNode = undefined;
-                            break;
-                        }
+        const index = hashCode % this.length;
+        if (this.array[index]) {
+            if (this.array[index].Element === element) {
+                this.array[index] = undefined;
+                this.count--;
+            } else {
+                while (this.array[index].NextNode) {
+                    if (this.array[index].NextNode === element) {
+                        this.array[index].NextNode = undefined;
+                        break;
                     }
-                    this.count--;
                 }
-            }
-        } else {
-            if (this.array[hashCode]) {
-                if (this.array[hashCode].Element === element) {
-                    this.array[hashCode] = undefined;
-                    this.count--;
-                } else {
-                    while (this.array[hashCode].NextNode) {
-                        if (this.array[hashCode].NextNode === element) {
-                            this.array[hashCode].NextNode = undefined;
-                            break;
-                        }
-                    }
-                    this.count--;
-                }
+                this.count--;
             }
         }
     }
 
     contains(element) {
         const hashCode = this.hashCode(element);
-        if (hashCode > this.length) {
-            const index = hashCode % this.length;
-            if (this.array[index]) {
-                if (this.array[index].Element === element) {
-                    return true;
-                } else {
-                    while (this.array[index].NextNode) {
-                        if (this.array[index].NextNode === element) {
-                            return true;
-                        }
-                    }
-                }
-            }
-        } else {
-            if (this.array[hashCode]) {
-                if (this.array[hashCode].Element === element) {
-                    return true;
-                } else {
-                    while (this.array[hashCode].NextNode) {
-                        if (this.array[hashCode].NextNode === element) {
-                            return true;
-                        }
+        const index = hashCode % this.length;
+        if (this.array[index]) {
+            if (this.array[index].Element === element) {
+                return true;
+            } else {
+                while (this.array[index].NextNode) {
+                    if (this.array[index].NextNode === element) {
+                        return true;
                     }
                 }
             }
         }
-
         return false;
     }
 }
